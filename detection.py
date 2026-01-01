73d39926e1a92e1530c7855749f9ba019f734c64
Karthik-coder2007 <karthik952007@gmail.com>
2026-01-01 19:46:58 +0530

    Remove unused cv2.rectangle code
    Removed commented-out rectangle drawing code.

--- detection.py
+++ detection.py
@@ -32,7 +32,6 @@ while True:
             # Bounding Box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
             w, h = x2 - x1, y2 - y1
             cvzone.cornerRect(img, (x1, y1, w, h))
             # Confidence
